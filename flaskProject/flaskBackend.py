4f908457bbedf5b08e0b819e9e88b635f939ca0d
Muhammad <muhammadrajput99@gmail.com>
2021-05-05 11:26:54 -0400

    finialized project

--- flaskProject/flaskBackend.py
+++ flaskProject/flaskBackend.py
@@ -18,7 +18,7 @@ from flask import session
 import bcrypt
 app = Flask(__name__)     # create an app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_event_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SE3155'
 db.init_app(app)
